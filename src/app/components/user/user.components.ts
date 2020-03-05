b03003749ed8f0f794d6f3dc45bdfed7433e8f5c
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-05 12:12:11 +0100

    add methods

--- src/app/components/user/user.components.ts
+++ src/app/components/user/user.components.ts
@@ -5,11 +5,26 @@ import { Component } from '@angular/core';
     templateUrl:"./user.component.html",
     styleUrls:["./user.component.css"],
     // styles:[]
-  
+
     // // template: "<h2>John Doe</h2>"
 })
 
 export class UserComponent {
-    constructor() { }
+  // properties
+  firstName ="John";
+  lastName="Doe";
+  age=30;
+
+  // Methods
+    constructor() {
+      console.log("Hello User...");
+
+     }
+
+     sayHello(){
+       console.log(`Hello ${this.firstName}` );
+       // same log bevor, but older syntax
+       console.log("Hello" + "" + this.firstName);
+     }
 
 }
