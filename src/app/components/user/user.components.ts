9aa9bc613986cfb26ab898ed29a3846f300a5488
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-05 15:01:34 +0100

    13/next

--- src/app/components/user/user.components.ts
+++ src/app/components/user/user.components.ts
@@ -1,30 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-user',
-    templateUrl:"./user.component.html",
-    styleUrls:["./user.component.css"],
-    // styles:[]
+  selector: 'app-user',
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
+  // styles:[]
 
-    // // template: "<h2>John Doe</h2>"
+  // // template: "<h2>John Doe</h2>"
 })
 
 export class UserComponent {
   // properties
-  firstName ="John";
-  lastName="Doe";
-  age=30;
+  firstName = "John";
+  lastName = "Doe";
+  age = 30;
 
   // Methods
-    constructor() {
-      console.log("Hello User...");
+  constructor() {
+    console.log("Hello User...");
+    this.hasBirthday();
+    this.sayHello();
+  }
 
-     }
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+    // same log bevor, but older syntax
+    console.log("Hello" + "" + this.firstName);
+  }
 
-     sayHello(){
-       console.log(`Hello ${this.firstName}` );
-       // same log bevor, but older syntax
-       console.log("Hello" + "" + this.firstName);
-     }
+  hasBirthday() {
+    this.age += 1;
 
+  }
 }
